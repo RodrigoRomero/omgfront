d5988a92c10ac72a3049a65418abe8413038f721
Rodrigo Romero <construirweb@hotmail.com>
2017-08-08 10:28:41 -0300

    add ga tracker

--- application/views/layout/head.php
+++ application/views/layout/head.php
@@ -54,6 +54,15 @@ fbq('init', '1748104025451888');
 fbq('track', 'PageView');
 
 </script>
-
 <!-- End Facebook Pixel Code -->
 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-104142848-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
