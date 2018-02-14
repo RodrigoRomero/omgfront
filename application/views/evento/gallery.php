170cbf873e664d7a1fb3fd93f14674e2ed6399c0
Rodrigo Romero <construirweb@hotmail.com>
2018-02-14 10:29:14 -0300

    change gallery layout

--- application/views/evento/gallery.php
+++ application/views/evento/gallery.php
@@ -34,224 +34,25 @@
 		</div><!-- #page-menu end -->
 				<!-- Portfolio Items
 				============================================= -->
-				<div id="portfolio" class="portfolio grid-container portfolio-nomargin portfolio-full portfolio-masonry mixed-masonry grid-container clearfix">
-
-					<article class="portfolio-item pf-media pf-icons wide">
-						<div class="portfolio-image">
-							<a href="portfolio-single.html">
-								<img src="uploads/gallery/thumbs/1.jpg" alt="Open Imagination">
-							</a>
-							<div class="portfolio-overlay">
-								<div class="portfolio-desc">
-									<h3><a href="portfolio-single.html">Open Imagination</a></h3>
-									<span><a href="#">Media</a>, <a href="#">Icons</a></span>
-								</div>
-								<a href="uploads/gallery/original/1.jpg" class="left-icon" data-lightbox="image"><i class="icon-line-plus"></i></a>
-
-							</div>
-						</div>
-					</article>
-
-					<article class="portfolio-item pf-illustrations">
-						<div class="portfolio-image">
-							<a href="portfolio-single.html">
-								<img src="uploads/gallery/thumbs/2.jpg" alt="Locked Steel Gate">
-							</a>
-							<div class="portfolio-overlay">
-								<div class="portfolio-desc">
-									<h3><a href="portfolio-single.html">Locked Steel Gate</a></h3>
-									<span><a href="#">Illustrations</a></span>
-								</div>
-								<a href="uploads/gallery/original/2.jpg" class="left-icon" data-lightbox="image"><i class="icon-line-plus"></i></a>
-
-							</div>
-						</div>
-					</article>
-
-					<article class="portfolio-item pf-graphics pf-uielements">
-						<div class="portfolio-image">
-							<a href="#">
-								<img src="uploads/gallery/thumbs/3.jpg" alt="Mac Sunglasses">
-							</a>
-							<div class="portfolio-overlay">
-								<div class="portfolio-desc">
-									<h3><a href="portfolio-single-video.html">Mac Sunglasses</a></h3>
-									<span><a href="#">Graphics</a>, <a href="#">UI Elements</a></span>
-								</div>
-								<a href="http://vimeo.com/89396394" class="left-icon" data-lightbox="iframe"><i class="icon-line-play"></i></a>
-
-							</div>
-						</div>
-					</article>
-
-					<article class="portfolio-item pf-icons pf-illustrations wide">
-						<div class="portfolio-image">
-							<div class="fslider" data-arrows="false" data-speed="400" data-pause="4000">
-								<div class="flexslider">
-									<div class="slider-wrap">
-										<div class="slide"><a href="portfolio-single-gallery.html"><img src="uploads/gallery/thumbs/4.jpg" alt="Morning Dew"></a></div>
-										<div class="slide"><a href="portfolio-single-gallery.html"><img src="uploads/gallery/thumbs/4-1.jpg" alt="Morning Dew"></a></div>
-									</div>
-								</div>
-							</div>
-							<div class="portfolio-overlay" data-lightbox="gallery">
-								<div class="portfolio-desc">
-									<h3><a href="portfolio-single-gallery.html">Morning Dew</a></h3>
-									<span><a href="#"><a href="#">Icons</a>, <a href="#">Illustrations</a></span>
-								</div>
-								<a href="uploads/gallery/original/4.jpg" class="left-icon" data-lightbox="gallery-item"><i class="icon-line-stack-2"></i></a>
-								<a href="uploads/gallery/original/4-1.jpg" class="hidden" data-lightbox="gallery-item"></a>
-
-							</div>
-						</div>
-					</article>
-
-					<article class="portfolio-item pf-uielements pf-media">
-						<div class="portfolio-image">
-							<a href="portfolio-single.html">
-								<img src="uploads/gallery/thumbs/5.jpg" alt="Console Activity">
-							</a>
-							<div class="portfolio-overlay">
-								<div class="portfolio-desc">
-									<h3><a href="portfolio-single.html">Console Activity</a></h3>
-									<span><a href="#">UI Elements</a>, <a href="#">Media</a></span>
-								</div>
-								<a href="uploads/gallery/original/5.jpg" class="left-icon" data-lightbox="image"><i class="icon-line-plus"></i></a>
-
-							</div>
-						</div>
-					</article>
-
-					<article class="portfolio-item pf-graphics pf-illustrations">
-						<div class="portfolio-image">
-							<div class="fslider" data-arrows="false">
-								<div class="flexslider">
-									<div class="slider-wrap">
-										<div class="slide"><a href="portfolio-single-gallery.html"><img src="uploads/gallery/thumbs/6.jpg" alt="Shake It"></a></div>
-										<div class="slide"><a href="portfolio-single-gallery.html"><img src="uploads/gallery/thumbs/6-1.jpg" alt="Shake It"></a></div>
-										<div class="slide"><a href="portfolio-single-gallery.html"><img src="uploads/gallery/thumbs/6-2.jpg" alt="Shake It"></a></div>
-										<div class="slide"><a href="portfolio-single-gallery.html"><img src="uploads/gallery/thumbs/6-3.jpg" alt="Shake It"></a></div>
-									</div>
-								</div>
-							</div>
-							<div class="portfolio-overlay" data-lightbox="gallery">
-								<div class="portfolio-desc">
-									<h3><a href="portfolio-single-gallery.html">Shake It!</a></h3>
-									<span><a href="#">Illustrations</a>, <a href="#">Graphics</a></span>
-								</div>
-								<a href="uploads/gallery/original/6.jpg" class="left-icon" data-lightbox="gallery-item"><i class="icon-line-stack-2"></i></a>
-								<a href="uploads/gallery/original/6-1.jpg" class="hidden" data-lightbox="gallery-item"></a>
-								<a href="uploads/gallery/original/6-2.jpg" class="hidden" data-lightbox="gallery-item"></a>
-								<a href="uploads/gallery/original/6-3.jpg" class="hidden" data-lightbox="gallery-item"></a>
-
-							</div>
-						</div>
-					</article>
-
-					<article class="portfolio-item pf-uielements pf-icons">
-						<div class="portfolio-image">
-							<a href="portfolio-single-video.html">
-								<img src="uploads/gallery/thumbs/7.jpg" alt="Backpack Contents">
-							</a>
-							<div class="portfolio-overlay">
-								<div class="portfolio-desc">
-									<h3><a href="portfolio-single-video.html">Backpack Contents</a></h3>
-									<span><a href="#">UI Elements</a>, <a href="#">Icons</a></span>
-								</div>
-								<a href="http://www.youtube.com/watch?v=kuceVNBTJio" class="left-icon" data-lightbox="iframe"><i class="icon-line-play"></i></a>
-
-							</div>
-						</div>
-					</article>
-
-					<article class="portfolio-item pf-graphics">
-						<div class="portfolio-image">
-							<a href="portfolio-single.html">
-								<img src="uploads/gallery/thumbs/8.jpg" alt="Sunset Bulb Glow">
-							</a>
-							<div class="portfolio-overlay">
-								<div class="portfolio-desc">
-									<h3><a href="portfolio-single.html">Sunset Bulb Glow</a></h3>
-									<span><a href="#">Graphics</a></span>
-								</div>
-								<a href="uploads/gallery/original/8.jpg" class="left-icon" data-lightbox="image"><i class="icon-line-plus"></i></a>
-
-							</div>
-						</div>
-					</article>
-
-					<article class="portfolio-item pf-illustrations pf-icons wide">
-						<div class="portfolio-image">
-							<div class="fslider" data-arrows="false" data-speed="650" data-pause="3500" data-animation="fade">
-								<div class="flexslider">
-									<div class="slider-wrap">
-										<div class="slide"><a href="portfolio-single-gallery.html"><img src="uploads/gallery/thumbs/9.jpg" alt="Bridge Side"></a></div>
-										<div class="slide"><a href="portfolio-single-gallery.html"><img src="uploads/gallery/thumbs/9-1.jpg" alt="Bridge Side"></a></div>
-										<div class="slide"><a href="portfolio-single-gallery.html"><img src="uploads/gallery/thumbs/9-2.jpg" alt="Bridge Side"></a></div>
-									</div>
-								</div>
-							</div>
-							<div class="portfolio-overlay" data-lightbox="gallery">
-								<div class="portfolio-desc">
-									<h3><a href="portfolio-single.html">Bridge Side</a></h3>
-									<span><a href="#">Illustrations</a>, <a href="#">Icons</a></span>
-								</div>
-								<a href="uploads/gallery/original/9.jpg" class="left-icon" data-lightbox="gallery-item"><i class="icon-line-stack-2"></i></a>
-								<a href="uploads/gallery/original/9-1.jpg" class="hidden" data-lightbox="gallery-item"></a>
-								<a href="uploads/gallery/original/9-2.jpg" class="hidden" data-lightbox="gallery-item"></a>
-
-							</div>
-						</div>
-					</article>
-
-					<article class="portfolio-item pf-graphics pf-media pf-uielements">
-						<div class="portfolio-image">
-							<a href="portfolio-single-video.html">
-								<img src="uploads/gallery/thumbs/10.jpg" alt="Study Table">
-							</a>
-							<div class="portfolio-overlay">
-								<div class="portfolio-desc">
-									<h3><a href="portfolio-single-video.html">Study Table</a></h3>
-									<span><a href="#">Graphics</a>, <a href="#">Media</a></span>
-								</div>
-								<a href="http://vimeo.com/91973305" class="left-icon" data-lightbox="iframe"><i class="icon-line-play"></i></a>
-
-							</div>
-						</div>
-					</article>
+				<div id="portfolio" class="portfolio grid-container portfolio-3 clearfix">
 
 					<article class="portfolio-item pf-media pf-icons">
-						<div class="portfolio-image">
-							<a href="portfolio-single.html">
-								<img src="uploads/gallery/thumbs/11.jpg" alt="Workspace Stuff">
-							</a>
-							<div class="portfolio-overlay">
-								<div class="portfolio-desc">
-									<h3><a href="portfolio-single.html">Workspace Stuff</a></h3>
-									<span><a href="#">Media</a>, <a href="#">Icons</a></span>
+							<div class="portfolio-image">
+								<a href="portfolio-single.html">
+									<img src="images/portfolio/3/1.jpg" alt="Open Imagination">
+								</a>
+								<div class="portfolio-overlay">
+									<a href="images/portfolio/full/1.jpg" class="center-icon" data-lightbox="image"><i class="icon-line-plus"></i></a>
 								</div>
-								<a href="uploads/gallery/original/11.jpg" class="left-icon" data-lightbox="image"><i class="icon-line-plus"></i></a>
-
 							</div>
-						</div>
-					</article>
-
-					<article class="portfolio-item pf-illustrations pf-graphics wide">
-						<div class="portfolio-image">
-							<a href="portfolio-single-gallery.html">
-								<img src="uploads/gallery/thumbs/12.jpg" alt="Workspace Stuff">
-							</a>
-							<div class="portfolio-overlay" data-lightbox="gallery">
-								<div class="portfolio-desc">
-									<h3><a href="portfolio-single-gallery.html">Fixed Aperture</a></h3>
-									<span><a href="#">Illustrations</a>, <a href="#">Graphics</a></span>
-								</div>
-								<a href="uploads/gallery/original/12.jpg" class="left-icon" data-lightbox="gallery-item"><i class="icon-line-stack-2"></i></a>
-								<a href="uploads/gallery/original/12-1.jpg" class="hidden" data-lightbox="gallery-item"></a>
-
+							<div class="portfolio-desc">
+								<h3><a href="portfolio-single.html">Open Imagination</a></h3>
+								<span><a href="#">Media</a>, <a href="#">Icons</a></span>
 							</div>
-						</div>
-					</article>
+						</article>
+
+
+
 
 				</div><!-- #portfolio end -->
 
