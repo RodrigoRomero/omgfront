81b898d1fb12f69d0d173a76c8b19fb59b7bfb54
Rodrigo Romero <construirweb@hotmail.com>
2018-03-02 10:23:49 -0300

    reorder menu

--- application/views/layout/main_navigation.php
+++ application/views/layout/main_navigation.php
@@ -3,9 +3,9 @@
 	<ul class="one-page-menu" data-easing="easeInOutExpo" data-speed="1500">
 		<li><a href="#" data-href="#home"><div>Home</div></a></li>
 		<!-- <li><a href="#" data-href="#section-agenda"><div>Agenda</div></a></li> -->
+		<li><a href="#" data-href="#section-tickets"><div>Tickets</div></a></li>
 		<li><a href="#" data-href="#section-oradores"><div>Oradores</div></a></li>
 		<li><a href="#" data-href="#section-sponsors"><div>Sponsors</div></a></li>
-		<li><a href="#" data-href="#section-tickets"><div>Tickets</div></a></li>
 		<li><a href="#" data-href="#section-lugar"><div>Lugar</div></a></li>
 		<li><a href="#" data-href="#section-gallery"><div>Galería</div></a></li>
 	</ul>
@@ -13,9 +13,9 @@
 	<ul >
 		<li><a href="<?php echo base_url('/#home') ?>"><div>Home</div></a></li>
 		<!-- <li><a href="<?php echo base_url('/#section-agenda') ?>" ><div>Agenda</div></a></li> -->
+		<li><a href="<?php echo base_url('/#section-tickets') ?>" ><div>Tickets</div></a></li>
 		<li><a href="<?php echo base_url('/#section-oradores') ?>" ><div>Oradores</div></a></li>
 		<li><a href="<?php echo base_url('/#section-sponsors') ?>" ><div>Sponsors</div></a></li>
-		<li><a href="<?php echo base_url('/#section-tickets') ?>" ><div>Tickets</div></a></li>
 		<li><a href="<?php echo base_url('/#section-lugar') ?>" ><div>Lugar</div></a></li>
 		<li><a href="<?php echo base_url('/#section-gallery') ?>" ><div>Galería</div></a></li>
 	</ul>
