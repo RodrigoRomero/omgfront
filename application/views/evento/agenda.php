033e3e1c40a8ff49b7e352bba37b835e3550ee30
RodrigoRomero <rodrigo.romero@bennu.tv>
2022-06-10 14:29:10 -0300

    remove agenda

--- application/views/evento/agenda.php
+++ application/views/evento/agenda.php
@@ -9,6 +9,7 @@ $fecha_inicio_array = explode("-", $fecha_inicio[0]);
 $fecha_cierre_array = explode("-", $fecha_cierre[0]);
 
 ?>
+<?php if(count($agenda) >=1){ ?>
 <section id="section-agenda" class="page-section topmargin-lg">
 
 	<div class="">
@@ -27,13 +28,11 @@ $fecha_cierre_array = explode("-", $fecha_cierre[0]);
 
 
 		<?php
-			if(count($agenda) >=1){
+			
 			foreach($agenda as $k => $item){
 				$this->load->view('evento/agenda-item', ['position'=>$k, 'item' => $item]);
 			}
-			} else {
-				echo "<h3 class='text-center'>Próximamente</h3>";
-			}
+	
 		?>
 <div class="one-page-menu center">
  <a href="javascript:void(0)" data-href="#section-tickets"" class="button button-xlarge button-teal">Comprar Tickets</a></div>
@@ -44,3 +43,4 @@ $fecha_cierre_array = explode("-", $fecha_cierre[0]);
 
 
 </section>
+<?php } ?>
