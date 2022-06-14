11569c302aaaa2701d237e4d6222511eb1eebf3e
RodrigoRomero <rodrigo.romero@bennu.tv>
2022-06-14 09:34:35 -0300

    fix typo

--- application/views/evento/corte_a.php
+++ application/views/evento/corte_a.php
@@ -1,6 +1,6 @@
 <section id="" class="page-section topmargin-lg">
 	<div class="container text-center">		
-		<h3><b>Argentina Visión 2040</b> es el encuentro anual donde los principales referentes del agro y la innovación dan su visión acerca de las oportunidades y desafíos del mundo que vie</h3>
+		<h3><b>Argentina Visión 2040</b> es el encuentro anual donde los principales referentes del agro y la innovación dan su visión acerca de las oportunidades y desafíos del mundo que viene</h3>
 		<h3>Celebramos esta <b>10° Edición con un evento presencial exclusivo</b> para 200 personas,  donde académicos y empresarios líderes abordarán temas de coyuntura, tecnología y sustentabilidad con la mirada puesta en el corto y mediano plazo. En el cierre te invitamos a compartir un cóctel con oradores y empresarios del sector.<br/><b>Una oportunidad única para encontrarse, compartir ideas y negocios.</b><h3>
 	</div>
 </section>
