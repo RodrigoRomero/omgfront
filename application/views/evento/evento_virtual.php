5f532a2329d1bb55b34eb90826eecf9a49758541
RodrigoRomero <rodrigo.romero@bennu.tv>
2022-06-14 10:27:52 -0300

    evento virtual decrease fontsize

--- application/views/evento/evento_virtual.php
+++ application/views/evento/evento_virtual.php
@@ -1,4 +1,4 @@
-<section id="" class="page-section topmargin-lg">
+<section id="" class="page-section ">
 	<div class="container text-center">		
 		<h4>Argentina Visión 2040 se transmitirá en vivo y en directo para quienes no puedan asistir al evento. Inscribite en el siguiente link</h4>
 		<a href="https://bit.ly/3xV8mAp" target="_blank" class="button button-xlarge button-teal">Inscribite</a>
