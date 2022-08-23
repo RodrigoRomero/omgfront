28d009b070cfe3fd929860853e8d923efe9a3400
RodrigoRomero <rodrigo.romero@bennu.tv>
2022-08-23 16:34:28 -0300

    change typo

--- application/views/evento/evento_virtual.php
+++ application/views/evento/evento_virtual.php
@@ -1,7 +1,7 @@
 <section id="" class="page-section ">
 	<div class="container text-center">		
 		<h4>Argentina Visión 2040 se transmitirá en vivo y en directo para quienes no puedan asistir al evento.</h4>
-		<a href="https://www.revistachacra.com.ar/argentinavision2040/" target="_blank" class="button button-xlarge button-teal">Quiero verlo online</a>
+		<a href="https://www.revistachacra.com.ar/argentinavision2040/" target="_blank" class="button button-xlarge button-teal">Ver Evento en VIVO</a>
 	</div>
 </section>
 
